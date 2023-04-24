84ffc73dfea4fbfddd39da33b1a39502d8bec34c
RyotaTakaoka <r.takaoka03@gmail.com>
2023-04-24 09:48:13 +0900

    ベラスケスのホームページ

---
+++ image_change.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const thumbs= document.querySelectorAll('.thumb');
+thumbs.forEach(function(item, index){
+  item.onclick = function(){
+    document.getElementById('big-img').src = this.dataset.image;
+  }
+});
